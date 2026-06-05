1be2df845cc0b91cdd92911dd9a30d53fb082907
Jan Erhard <janferhard@gmail.com>
2026-06-05 10:38:20 +0200

    added initial cli support

---
+++ src/cli.rs
@@ -0,0 +1,70 @@
+use math_utils_lib::{Context, ExportType, Step, eval, export_history, parse};
+
+pub fn handle_expressions(expressions: Vec<String>) {
+    let mut context = Context::default();
+    let mut history = vec![];
+
+    for expression in expressions {
+        if expression.to_uppercase() == "EXPORT" || expression.to_uppercase() == "EXPORT --PDF" {
+            let pdf = export_history(history.clone(), ExportType::Pdf);
+
+            if let Ok(binary) = pdf {
+                if let Ok(_) = std::fs::write("export.pdf", binary) {} else {
+                    println!("Error exporting pdf!");
+                }
+            } else {
+                println!("Error exporting pdf!");
+            }
+
+            println!("Exported to export.pdf!");
+
+            continue;
+        }
+        if expression.to_uppercase() == "EXPORT --TEX" {
+            let pdf = export_history(history.clone(), ExportType::Tex);
+
+            if let Ok(binary) = pdf {
+                if let Ok(_) = std::fs::write("export.tex", binary) {} else {
+                    println!("Error exporting tex!");
+                }
+            } else {
+                println!("Error exporting tex!");
+            }
+
+            println!("Exported to export.tex!");
+
+            continue;
+        }
+        if expression.to_uppercase() == "EXPORT --PNG" {
+            let pdf = export_history(history.clone(), ExportType::Png);
+
+            if let Ok(binary) = pdf {
+                if let Ok(_) = std::fs::write("export.png", binary) {} else {
+                    println!("Error exporting png!");
+                }
+            } else {
+                println!("Error exporting png!");
+            }
+
+            println!("Exported to export.png!");
+
+            continue;
+        }
+
+        let parsed_expression = match parse(expression) {
+            Ok(ast) => ast,
+            Err(e) => {println!("{}", e.get_reason()); continue;}
+        };
+
+        let result = match eval(&parsed_expression, &mut context) {
+            Ok(result) => result,
+            Err(e) => {println!("{}", e.get_reason()); continue;}
+        };
+
+        let step = Step::new(parsed_expression, result);
+
+        println!("{}", step.as_string());
+
+        history.push(step);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,38 @@ use message_handler::handle_message;
 
 pub mod repl;
 pub mod message_handler;
+mod cli;
 
 pub use crate::repl::Repl;
-use crate::repl::State;
+use crate::{cli::handle_expressions, repl::State};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Expression(s) to evaluate
+    #[arg(short, long, required=false)]
+    eval: Option<Vec<String>>
+}
 
 pub fn main() {
-    let initial_state = State::new(Context::default());
-    let mut repl = Repl::new("-> ".to_string(), "   ".to_string(), initial_state, handle_message);
+    let args = Args::parse();
 
-    let _ = repl.run_repl();
+    match args.eval {
+        None => {
+            let initial_state = State::new(Context::default());
+            let mut repl = Repl::new("-> ".to_string(), "   ".to_string(), initial_state, handle_message);
+
+            let _ = repl.run_repl();
+        },
+        Some(expressions) if expressions.len() == 0 => {
+            let initial_state = State::new(Context::default());
+            let mut repl = Repl::new("-> ".to_string(), "   ".to_string(), initial_state, handle_message);
+
+            let _ = repl.run_repl();
+        },
+        Some(expressions) => {
+            handle_expressions(expressions);
+        }
+    }
 }
